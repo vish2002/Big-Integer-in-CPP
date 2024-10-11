0f7d9f12d51739cdf5299f33b3d162ded9275ae8
Vishal Kurve <vishalkurve7@gmail.com>
2024-10-11 23:43:13 +0530

    Create BigInteger.h

---
+++ BigInteger.h
@@ -0,0 +1,44 @@
+#ifndef BIGINTEGER_H
+#define BIGINTEGER_H
+
+#include <iostream>
+#include <string>
+
+// Node structure for linked list
+struct Node {
+    int data;
+    Node* next;
+};
+
+// BigInteger class
+class BigInteger {
+private:
+    Node* head;
+
+public:
+    // Constructor
+    BigInteger();
+
+    // Destructor
+    ~BigInteger();
+
+    // Function to add a new node at the end of the linked list
+    void addNode(int data);
+
+    // Function to display the linked list
+    void display();
+
+    // Function to calculate the factorial of a large number
+    void factorial(int n);
+
+    // Function to add two large numbers
+    void add(BigInteger& num2);
+
+    // Function to subtract two large numbers
+    void subtract(BigInteger& num2);
+
+    // Function to convert a string to a BigInteger
+    void stringToBigInteger(const std::string& str);
+};
+
+#endif // BIGINTEGER_H
